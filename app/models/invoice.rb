2a90a655cb5f4dc23f8f60f2c365781c1a1341d5
MacbookAir <macbookair@Tums-MacBook-Air.local>
2015-07-03 11:52:59 +0700

    update

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,32 @@ class Invoice < ActiveRecord::Base
   validates :number, presence: true
 
   accepts_nested_attributes_for :line_items
+  after_touch  :calculate_total
+  after_initialize :up_number, :if => :new_record?
+
+  def calculate_total
+      sum = 0
+      items = self.line_items.all
+      items.each do |item|
+        sum = sum + (item.quantity.to_i * item.price.to_i)
+      end
+      summation(sum)
+  end
+
+  def summation(sum)
+      self.total = sum
+      self.save
+  end
+
+  def up_number
+    unless number
+      if Invoice.count == 0
+        self.number = '001'
+      else
+        last_invoice = Invoice.order(:number).last
+        self.number = last_invoice.number.succ
+      end
+    end
+  end
+
 end
